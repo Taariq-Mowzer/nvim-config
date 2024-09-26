22411b7e88028506fa00e79b65a246785afb83df
Taariq Mowzer <taariq787898@gmail.com>
2024-09-26 10:50:13 +0200

    No longer user 2917 in latex

--- plugin/autopairs.lua
+++ plugin/autopairs.lua
@@ -4,8 +4,3 @@ autopairs.setup({})
 
 local Rule = require('nvim-autopairs.rule')
 local cond = require('nvim-autopairs.conds')
-
-autopairs.add_rules({
-    Rule("$", "$", "tex")
-        :with_move(cond.after_text("$") and cond.not_before_char("$")),
-})
