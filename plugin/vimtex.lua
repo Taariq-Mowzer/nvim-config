ac4e7670db40f353f3a2062c9b5608566d28659d
Taariq Mowzer <taariq787898@gmail.com>
2025-02-07 17:29:17 +0000

    Changed bibliography engine to biber

--- plugin/vimtex.lua
+++ plugin/vimtex.lua
@@ -1,4 +1,5 @@
 vim.g.vimtex_complete_enabled = 1
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_bibtex_flavor = 'biber'
 vim.g.vimtex_quickfix_ignore_filters = {"Underfull", "Overfull"}
