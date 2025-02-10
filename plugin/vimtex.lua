a1ba6de125957542e6b63816b510dd962bccf486
Taariq Mowzer <taariq787898@gmail.com>
2025-02-10 12:28:47 +0000

    aux latex files go to hidden dir

--- plugin/vimtex.lua
+++ plugin/vimtex.lua
@@ -3,3 +3,6 @@ vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.vimtex_bibtex_flavor = 'biber'
 vim.g.vimtex_quickfix_ignore_filters = {"Underfull", "Overfull"}
+vim.g.vimtex_compiler_latexmk = {
+    aux_dir = ".latex-tmp", -- create a directory called aux that will contain all the auxiliary files
+}
