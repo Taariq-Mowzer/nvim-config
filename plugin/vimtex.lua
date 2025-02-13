57ea4f24a1c2206d2b641568f08863e7aba6b21e
Taariq Mowzer <taariq787898@gmail.com>
2025-02-13 13:37:28 +0000

    Ignore font warnings for tex

--- plugin/vimtex.lua
+++ plugin/vimtex.lua
@@ -2,7 +2,7 @@ vim.g.vimtex_complete_enabled = 1
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.vimtex_bibtex_flavor = 'biber'
-vim.g.vimtex_quickfix_ignore_filters = {"Underfull", "Overfull"}
+vim.g.vimtex_quickfix_ignore_filters = {"Underfull", "Overfull", "LaTeX Font Warning:"}
 vim.g.vimtex_compiler_latexmk = {
     aux_dir = ".latex-tmp", -- create a directory called aux that will contain all the auxiliary files
 }
