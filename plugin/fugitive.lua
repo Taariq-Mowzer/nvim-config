1ad93891d9e413b823a6dad5a398ff9a23b76db4
Taariq Mowzer <taariq787898@gmail.com>
2025-03-15 12:19:00 +0000

    Added better highlighting for git commits
    The first 50 characters of the commit title are highlighted.
    Everything after is highlighted red.
    
    There is also an 80 character vertical line.

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -1,1 +1,11 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit" },
+  callback = function()
+    vim.cmd("setlocal spell")
+    vim.cmd("setlocal colorcolumn=80")
+	vim.cmd([[match Title /\%1l.\{,50}/]])  -- First 50 chars in 'Title' highlight
+    vim.cmd([[2match ErrorMsg /\%1l.\%>50c.*/]])  -- Beyond 50 chars in 'ErrorMsg'
+  end,
+})
