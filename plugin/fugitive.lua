be5b4ea92bbf5e8dbe5fbf8cd561240c9fd1fb80
Taariq Mowzer <taariq787898@gmail.com>
2025-03-18 11:58:59 +0000

    Change colorcolumn to adhere to git standards
    Vim by default wraps 72 characters for commit messages. The colorcolumn
    has been changed to match this.

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit" },
   callback = function()
     vim.cmd("setlocal spell")
-    vim.cmd("setlocal colorcolumn=80")
+    vim.cmd("setlocal colorcolumn=72")
 	vim.cmd([[match Title /\%1l.\{,50}/]])  -- First 50 chars in 'Title' highlight
     vim.cmd([[2match ErrorMsg /\%1l.\%>50c.*/]])  -- Beyond 50 chars in 'ErrorMsg'
   end,
