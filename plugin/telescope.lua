5581641cacfa6553b45b48a0b02e5afd0ee6a8db
Taariq <taariq.mowzer@signaloid.com>
2025-08-29 09:59:19 +0200

    Adds :LS to Telescope search buffers

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,2 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+vim.api.nvim_create_user_command("LS", function()
+  vim.cmd("Telescope buffers")
+end, {})
