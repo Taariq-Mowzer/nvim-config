6ad4a9ed30b8889b9a615fadc3ac30be107c9f08
Taariq <taariq.mowzer@signaloid.com>
2025-08-29 10:00:27 +0200

    Changes bindings for splits
    <leader> direction now changes focus of window.
    
    <leader> s(v/h) makes split direction: The order was changed so that
    <leader> h would only be used for direction change.

--- lua/taariq/remap.lua
+++ lua/taariq/remap.lua
@@ -1,7 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>vs", vim.cmd.vsplit)
-vim.keymap.set("n", "<leader>hs", vim.cmd.split)
+vim.keymap.set("n", "<leader>sv", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>sh", vim.cmd.split)
+
+-- Move between splits with <leader> + h/j/k/l
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
