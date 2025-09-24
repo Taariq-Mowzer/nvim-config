8da944a1ccf553362acef2e7f3b28ffdfef48c8f
Taariq <taariq.mowzer@signaloid.com>
2025-09-24 16:38:26 +0200

    Changes diagnostic reporting behaviour
    Diagnostics no longer show symbols in signcolumn.
    Diagnostics only colour the line number.

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -41,6 +41,28 @@ vim.lsp.enable('clang')
 -- Diagnostic should have rounded borders
 vim.o.winborder = 'rounded'
 
+-- Remove signs on line bar. Instead just highlight the linenumber
+vim.diagnostic.config({
+	severity_sort=true,
+	signs = {
+		text = {
+		[vim.diagnostic.severity.ERROR] = '',
+		[vim.diagnostic.severity.WARN] = '',
+		[vim.diagnostic.severity.HINT] = '',
+		[vim.diagnostic.severity.INFO] = '',
+		},
+	linehl = {
+	    --[vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+	},
+	numhl = {
+	    [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
+	    [vim.diagnostic.severity.WARN] = 'DiagnosticWarning',
+	    [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+	    [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+	},
+    },
+})
+
 -- Autocomplete maps
 vim.api.nvim_set_keymap(
   'n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>',
