a377235edd63d965d74ec7cd8eaa5d66a4f21200
Taariq <taariq.mowzer@signaloid.com>
2025-09-30 10:56:07 +0200

    Sorts telescope buffer by last used

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,6 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-
 vim.api.nvim_create_user_command("LS", function()
-  vim.cmd("Telescope buffers")
+  builtin.buffers({sort_mru = true})
 end, {})
